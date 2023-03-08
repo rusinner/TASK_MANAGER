d8e5525273cdcb1a73ce24f6ff5f3da5d3089d78
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-08 14:34:13 +0200

    setup routing

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
 
 //port server running
 const port = 3000;
+
+//middleware
+app.use(express.json());
 
 //routes
 app.get("/hello", (req, res) => {
   res.send("task manager app");
 });
+
+//routes middleware
+app.use("/api/v1/tasks", tasks);
 
 //listen to server
 app.listen(3000, console.log(`Server is listening on port:${port}...`));
