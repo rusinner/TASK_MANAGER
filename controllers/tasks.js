42032232ca283231355e8ebf78f52817d25d2e91
Theologos Mavropoulos <rusinner88@gmail.com>
2023-03-09 12:16:51 +0200

    setup custom 404

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,7 +5,11 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(200).json({ tasks });
+    // res.status(200).json({ tasks });
+    res.status(200).json({ tasks, amount: tasks.length });
+    // res
+    //   .status(200)
+    //   .json({ status: "success", data: { tasks, nbHits: tasks.length } });
   } catch (error) {
     res.status(500).json({ message: error });
   }
